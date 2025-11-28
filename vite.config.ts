354e040713b612743c94a8c921de6cba47693898
Sabertaz <sabertazimi@gmail.com>
2025-11-29 02:40:05 +0800

    feat(boilerplate): setup portfolio app (#2)
    * feat(boilerplate-vite): setup vite app project
    
    * feat(boilerplate-tailwindcss): add tailwindcss support
    
    * feat(boilerplate-shadcn/ui): add Shadcn UI support
    
    * ci(package.json): setup package manager version

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
