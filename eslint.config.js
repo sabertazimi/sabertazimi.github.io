ea2530484241d6641a2ba99c1c4bbc28fd3f8c15
sabertazimi <sabertazimi@gmail.com>
2025-12-20 01:07:02 +0800

    chore(deps): update `@dg-scripts` packages to v6.0.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,19 @@
-import eslintConfig from '@dg-scripts/eslint-config'
+import { defineConfig } from '@dg-scripts/eslint-config'
 
-export default eslintConfig.append({
-  files: ['src/components/ui/*.tsx'],
-  rules: {
-    'react/no-array-index-key': 'off',
-    'react/no-children-map': 'off',
-    'react/no-clone-element': 'off',
-    'react-dom/no-flush-sync': 'off',
-    'react-hooks/purity': 'off',
-    'react-hooks-extra/no-direct-set-state-in-use-effect': 'off',
-    'react-refresh/only-export-components': 'off',
-    'security/detect-object-injection': 'off',
-    'ts/strict-boolean-expressions': 'off',
+export default defineConfig(
+  { typescript: true },
+  {
+    name: 'react',
+    files: ['src/components/ui/*.tsx'],
+    rules: {
+      'react/no-array-index-key': 'off',
+      'react/no-children-map': 'off',
+      'react/no-clone-element': 'off',
+      'react-dom/no-flush-sync': 'off',
+      'react-hooks/purity': 'off',
+      'react-hooks-extra/no-direct-set-state-in-use-effect': 'off',
+      'react-refresh/only-export-components': 'off',
+      'ts/strict-boolean-expressions': 'off',
+    },
   },
-},
 )
